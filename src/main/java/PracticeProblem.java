c7e73f6e347dcff0f8e400d015adb3f3d63f7d6a
michael-chen-ics4u1-01-25 <michael.chen29@student.tdsb.on.ca>
2025-09-30 14:54:04 -0400

    completed set

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
+	public static int goodString(String a) {
+		ArrayList<String> stringChars = new ArrayList<String>();
+		ArrayList<Integer> moves = new ArrayList<Integer>();
+		for (String i : a.split("")) {
+			if (!stringChars.contains(i)) {
+				stringChars.add(i);
+			}
+		}
+
+		if (stringChars.size() == a.length()) {
+			return -1;
+		}
+
+		for (String j : stringChars) {
+			moves.add(a.indexOf(j) + new StringBuilder(a).reverse().toString().indexOf(j));
+		}
+		return Collections.min(moves);
+	}
 }
